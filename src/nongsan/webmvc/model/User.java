7b5fc5f09684ab8310e2a08eb2b76427f679c36f
Vy <kimvy219@gmail.com>
2020-05-20 11:43:50 +0700

    Add model user

---
+++ src/nongsan/webmvc/model/User.java
@@ -0,0 +1,71 @@
+package nongsan.webmvc.model;
+import java.util.Date;
+
+public class User{
+	private int id;
+	private String name;
+	private String email;
+	private String phone;
+	private String username;
+	private String password;
+	private Date created;
+	public User(int id, String name, String email, String phone, String username, String password, Date created) {
+		super();
+		this.id = id;
+		this.name = name;
+		this.email = email;
+		this.phone = phone;
+		this.username = username;
+		this.password = password;
+		this.created = created;
+	}
+	public int getId() {
+		return id;
+	}
+	public void setId(int id) {
+		this.id = id;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public String getEmail() {
+		return email;
+	}
+	public void setEmail(String email) {
+		this.email = email;
+	}
+	public String getPhone() {
+		return phone;
+	}
+	public void setPhone(String phone) {
+		this.phone = phone;
+	}
+	public String getUsername() {
+		return username;
+	}
+	public void setUsername(String username) {
+		this.username = username;
+	}
+	public String getPassword() {
+		return password;
+	}
+	public void setPassword(String password) {
+		this.password = password;
+	}
+	public Date getCreated() {
+		return created;
+	}
+	public void setCreated(Date created) {
+		this.created = created;
+	}
+	@Override
+	public String toString() {
+		return "User [id=" + id + ", name=" + name + ", email=" + email + ", phone=" + phone + ", username=" + username
+				+ ", password=" + password + ", created=" + created + "]";
+	}
+	
+	
+}
